49a3b5eb4db77c37e5af1c662039ec4fe5bdb699
wsqy <1323800521@qq.com>
2017-12-09 00:14:52 +0800

    服务端章节接口升级

--- server/book/views.py
+++ server/book/views.py
@@ -84,6 +84,15 @@ class ChapterViewset(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.Retr
     retrieve:
         章节详情
     """
+    def create(self, request, *args, **kwargs):
+        _data = request.data.copy()
+        _data['article'] = self.kwargs.get('article_id')
+        serializer = self.get_serializer(data=_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
     def get_queryset(self):
         queryset = Chapter.objects.filter(article=self.kwargs.get('article_id'))
         return queryset
