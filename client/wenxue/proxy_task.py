98e705989b1899c395468b67b8e412b7452ef813
wsqy <1323800521@qq.com>
2017-12-09 21:07:57 +0800

    爬取首页完成

--- client/wenxue/proxy_task.py
+++ client/wenxue/proxy_task.py
@@ -18,60 +18,47 @@ red_conf = {
     'REDIS_PASSWD': settings.REDIS_PASSWD,
 }
 
-# red = RedisObj(**red_conf)
-# redis_push_key = "%s:%s:%s"
-# key = redis_push_key % (settings.REDIS_KEY_ARTICLE, 1, 2)
+red = RedisObj(**red_conf)
+redis_push_key = "%s:%s:%s"
+article_key = redis_push_key % (settings.REDIS_KEY_ARTICLE, 1, 2)
+article_list_key = redis_push_key % (settings.REDIS_KEY_ARTICLE_LIST, 1, 2)
 # r = red.get_task(key)
 # print(r)
 # print(json.loads(r))
 
 def main():
     count = 0
-    while count < 1:
-        # r = red.get_task(key)
-        r = {
-            "id": 2546,
-            "url": 'http://t66y.com/htm_data/20/1712/2829288.html',
-        }
-        r = json.dumps(r)
+    while count < 100:
+        r = red.get_task(article_key)
+        # r = {
+        #     "id": 2546,
+        #     "url": 'http://t66y.com/htm_data/20/1712/2829288.html',
+        # }
+        # r = json.dumps(r)
         if(r):
-            # dic_data = {
-            #     "page": 1,
-            #     "title": "暑假上了大学宿舍的美女少妇楼管",
-            #     "content": "skjj",
-            #     "url": "http://t66y.com/htm_data/20/1712/2829288.html",
-            #     "create_time": "2017-12-08 09:56",
-            #     "floor": 0
-            # }
-            # r = requests.post( settings.WENXUE_ARTICLE_CHAPTER_URL % '2546', json = json.dumps(dic_data, ensure_ascii=False))
-            # print("111111")
-            # print(r.status_code)
-            # print("111111")
-            # print(r.text)
-            # print("111111")
-            # return
             r = json.loads(r)
             dic = get_first_page(r.get('url'))
-            dic_data_list = dic.get("data_list")
-            for dic_data in dic_data_list:
+            for dic_data in dic.get("data_list"):
                 dic_data['url'] = r.get('url')
                 dic_data['page'] = 1
                 # dic_data['content'] = 'content'
                 print("++++++")
-                print(json.dumps(dic_data, ensure_ascii=False))
-                print("++++++222")
+                # print(dic_data)
+                print("++++++")
                 _url = settings.WENXUE_ARTICLE_CHAPTER_URL % str(r.get('id'))
                 print(_url)
                 req = requests.post( _url, data = dic_data)
                 # r = requests.get( _url)
                 print(req.status_code)
-                print("++++++111")
-                # print(req.text)
-                with open('req_text.html', 'w') as f:
-                    f.write(req.text)
                 print("++++++")
-                # print(req.headers)
-                print("++++++")
+            for other_url in dic.get("other_url_list"):
+                dic = {
+                    'id': r.get('id'),
+                    'url': other_url
+                }
+                res = red.push_task(article_list_key, json.dumps(dic))
+                print("%s----%s" % (res, dic.get('url')))
+
             # print(dic.get('data_list').get(''))1
             count += 1
             time.sleep(random.randint(1, 5))
